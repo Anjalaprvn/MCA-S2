177729ff9810d223842b33e22b121f4abe00e608
ANJALA PARVEEN <116816862+Anjalaprvn@users.noreply.github.com>
2025-04-15 11:56:13 +0530

    Update Program9.sh

--- shell/Record/Program9.sh
+++ shell/Record/Program9.sh
@@ -5,3 +5,13 @@ read n
 sum=$(( n * (n + 1) / 2 ))
 
 echo "Sum = $sum"
+
+
+Algorithm
+Step 1: Start the program
+Step 2: Display the message: "Enter a number:"
+Step 3: Read the number and store it in variable n
+Step 4: Calculate the sum of the first n natural numbers using the formula:
+        sum=𝑛(𝑛+1)/2  and store the result in variable sum
+Step 5: Display the message: "Sum = $sum"
+Step 6: End the program
