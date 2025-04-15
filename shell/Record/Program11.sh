520aa7ae29eb14cfc21be1cd51226756447a13ac
ANJALA PARVEEN <116816862+Anjalaprvn@users.noreply.github.com>
2025-04-15 12:00:38 +0530

    Update Program11.sh

--- shell/Record/Program11.sh
+++ shell/Record/Program11.sh
@@ -7,3 +7,16 @@ do
   echo "$num x $i = $((num * i))"
   i=$((i + 1))
 done
+
+
+Algorithm:
+Step1:Start the program
+Step2:Display the message: "Display the Multiplication table of:"
+Step3:Read the number and store it in variable num
+Step4:Initialize a counter variable i = 1
+Step5:Start a while loop with the condition i <= 10
+      -Display the multiplication expression: "num x i = num * i"
+      -Increment i by 1
+Step6:End the loop when i becomes greater than 10
+Step7:End the program
+
